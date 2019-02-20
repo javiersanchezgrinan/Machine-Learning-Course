6487fb037520c90e23346bf0e43f42fdea4a11a0
SYSTEM <javiersanchez_5@hotmail.com>
2019-02-20 20:00:42 +0100

    LAB 5 PREDICT FUNCTION

---
+++ ML_5.R
@@ -0,0 +1,8 @@
+'ENUNCIADO'
+set.seed(1)
+n <- 100
+Sigma <- 9*matrix(c(1.0, 0.5, 0.5, 1.0), 2, 2)
+dat <- MASS::mvrnorm(n = 100, c(69, 69), Sigma) %>%
+  data.frame() %>% setNames(c("x", "y"))
+'Q1'
+
