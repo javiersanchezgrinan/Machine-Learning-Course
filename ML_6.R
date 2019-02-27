68e4bc78d40e9f55824148bea128cc96ce50819a
SYSTEM <javiersanchez_5@hotmail.com>
2019-02-27 11:25:17 +0100

    intentando lab 6

--- ML_6.R
+++ ML_6.R
@@ -14,3 +14,29 @@ make_data <- function(n = 1000, p = 0.5,
 }
 dat <- make_data()
 head(dat)
+
+'se trata de crear una diferencia numerica entre un conjunto de datos y otro e ir viendo como el accuracy va creciendo a medidad 
+que se van distanciando ambas muestras normales (no hay intersección de datos)'
+delta <- seq(0, 3, len=25)
+accuracy <- map_dbl(delta, function(x){
+  
+  y <- rbinom(1000, 1, 0.5)
+  f_0 <- rnorm(1000, 0, 1)
+  f_1 <- rnorm(1000, 2, 1)
+  x <- ifelse(y == 1, f_1, f_0)
+  test_index <- createDataPartition(y, times = 1, p = 0.5, list = FALSE)
+  train0 <- data.frame(x = x, y = as.factor(y))
+  train <- train0 %>% slice(-test_index)
+  test0 <- data.frame(x = x, y = as.factor(y))
+  test <- test0 %>% slice(test_index)
+  glm_fit <- train %>% glm(y~x,data=.,family = "binomial")
+  glm_fit
+  p_hat_logit <- predict(glm_fit, newdata = test, type = "response")
+  p_hat_logit
+  y_hat <- factor(ifelse(p_hat_logit > 0.5, 1, 0))
+  y_hat
+  mean(y_hat==test$y)
+                                        })
+
+
+
